def28f171de113c646fa5d98d0c5aea322f65344
Ga1oker <54763540+Galoker1@users.noreply.github.com>
2021-01-20 13:15:11 +0300

    Add files via upload

--- laba-7/script.js
+++ laba-7/script.js
@@ -1,10 +1,11 @@
-
-
-
-async function get_all(){
+async function get_data(){
         let res = await fetch("http://127.0.0.1:8000/get_all",({method:'GET'}));
         let content = await res.text();
         let data = JSON.parse(content);
+        return data
+
+function get_all(){
+        data=get_data()
         document.querySelector('.content').innerHTML= "<button type=\"button\" class=\"btn btn-success\" onclick='create()' style=\"float:left;\">Создать</button> <table class=\"table table-dark table-striped\" id=\"tabl\" style=\"width: 80%;margin-left: 1%;margin-top:80px;float:left;\">\n" +
             "        <tr>\n" +
             "            <td>Имя</td>\n" +
@@ -18,20 +19,10 @@ async function get_all(){
             "\n" +
             "    </table>"
         for (let i=0;i < data.length;i++){
-
-       		document.querySelector('tbody').innerHTML+="<tr><td> "+ data[i]["Имя"] + " </td> <td>"+  data[i]["Фамилия"] + "</td> <td> " + data[i]["E-mail"] + " </td> <td> " + data[i]["Телефон"]+"</td>"+"<td><button type=\"button\" class=\"btn btn-danger\" style=\"float:left;margin-left:10px;\" onclick=\"del(" + i + ")\">Удалить</button></td>"+"<td><button type=\"button\" class=\"btn btn-warning\" style=\"float:left;margin-left:10px;\" onclick=\"edit(" + i + ")\">Редактировать</button></td></tr>";
-
-        }}
-
-function send_new(name,surname,email,phone){
-
-        let req = fetch("http://127.0.0.1:8000/create?name="+name+"&surname="+surname+"&email="+email+"&phone="+phone,({method: 'POST'}));
-        get_all()
-
-}
+				document.querySelector('tbody').innerHTML+="<tr><td> "+ data[i]["Имя"] + " </td> <td>"+  data[i]["Фамилия"] + "</td> <td> " + data[i]["E-mail"] + " </td> <td> " + data[i]["Телефон"]+"</td>"+"<td><button type=\"button\" class=\"btn btn-danger\" style=\"float:left;margin-left:10px;\" onclick=\"del(" + i + ")\">Удалить</button></td>"+"<td><button type=\"button\" class=\"btn btn-warning\" style=\"float:left;margin-left:10px;\" onclick=\"edit(" + i + ")\">Редактировать</button></td></tr>";
+			}}
 function create(){
-
-        document.querySelector('.content').innerHTML=
+	document.querySelector('.content').innerHTML=
             "<button type=\"button\" class=\"btn btn-danger\" onclick='get_all()'>Отмена</button>"+
             "<div class=\"form-floating mb-3\" style='margin-top: 20px;'>\n" +
             "  <input type=\"email\" class=\"form-control\"  id=\"name\"  placeholder=\"name@example.com\">\n" +
@@ -50,22 +41,17 @@ function create(){
         "  <label for=\"phone\" id='phone'>Телефон</label>\n" +
           "<button type=\"button\" class=\"btn btn-success\" style=\"margin-top:20px;\" onclick='send_new(document.getElementById(\"name\").value,document.getElementById(\"surname\").value,document.getElementById(\"email\").value,document.getElementById(\"phone\").value)'>Создать</button>"+
         "</div>";
-
 }
-async function del(i){
-	       let res = await fetch("http://127.0.0.1:8000/get_all",({method:'GET'}));
-        let content = await res.text();
-        let data = JSON.parse(content);
-    let req = fetch("http://127.0.0.1:8000/delete?id="+i,({method:'POST'}));
+function send_new(name,surname,email,phone){
+		let req = fetch("http://127.0.0.1:8000/create?name="+name+"&surname="+surname+"&email="+email+"&phone="+phone,({method: 'POST'}));
+        get_all()
+}
+function del(i){
+	let req = fetch("http://127.0.0.1:8000/delete?id="+i,({method:'POST'}));
 	get_all();
-
 }
-
-
-async function edit(i){
-		let res = await fetch("http://127.0.0.1:8000/get_all",({method:'GET'}));
-        let content = await res.text();
-        let data = JSON.parse(content);
+function edit(i){
+		data=get_data()
         document.querySelector('.content').innerHTML=
             "<button type=\"button\" class=\"btn btn-danger\" onclick='get_all()'>Отмена</button>"+
             "<div class=\"form-floating mb-3\" style='margin-top: 20px;'>\n" +
@@ -90,7 +76,7 @@ async function edit(i){
         document.getElementById("email").value=data[i]["E-mail"];
         document.getElementById("phone").value=data[i]["Телефон"];
 }
-async function true_edit(i,name,surname,email,phone){
+function true_edit(i,name,surname,email,phone){
         fetch("http://127.0.0.1:8000/update?name="+name+"&surname="+surname+"&email="+email+"&phone="+phone+"&id="+i,({method:"POST"}))
         get_all();
 
